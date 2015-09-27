cb4aa32dcbc83d815e20bb70bfa470e920c6f542
Eric Ross <eric.ross@hp.com>
2015-09-26 20:16:58 -0700

    initial xcode project

---
+++ naqcclogger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  naqcclogger
+//
+//  Created by Eric Ross on 9/26/15.
+//  Copyright © 2015 Eric Ross. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
